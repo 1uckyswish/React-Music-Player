59c32e9857ecfa89672bf919c8394a4e9cd75bc0
twentyfive21 <tina.winnn@gmail.com>
2023-10-05 15:43:43 -0400

    mapped out data

--- src/Components/SongCard/SongCard.jsx
+++ src/Components/SongCard/SongCard.jsx
@@ -1,22 +1,21 @@
 import React from 'react'
 import './SongCard.css'
-import song from '../../assets/cover-1.jpg'
 import { MdOutlineAdd } from "react-icons/md";
 import { AiOutlineClose } from "react-icons/ai";
 
 
 
-function SongCard({Queue}) {
+function SongCard({Queue, song}) {
   if(Queue){
     console.log('hii')
   }
   return (
     <section className='playlist-row'>
     <div className='playlist-container'>
-        <img src={song} alt='song thumbnail' />
+        <img src={song?.Thumbnail} alt='song thumbnail' />
         <div className='playlist-info'>
-            <h1>Either Way</h1>
-            <h2>Ive</h2>
+            <h1>{song?.Title}</h1>
+            <h2>{song?.Artist}</h2>
         </div>
     </div>
     <div className='playlist-edit'>
