11fd05c6e9923be2bde7a0c0d055428c3bd20a05
twentyfive21 <tina.winnn@gmail.com>
2023-10-14 22:43:19 -0400

    added slider for song

--- src/Components/PlayingNext/PlayingNext.jsx
+++ src/Components/PlayingNext/PlayingNext.jsx
@@ -1,41 +1,86 @@
-import song from '../../assets/cover-1.jpg'
-import React, { useContext } from "react";
-import { GET_QUEUED_SONGS } from '../../utils/queries';
+import song from "../../assets/cover-1.jpg";
+import React, { useContext, useRef, useState, } from "react";
+import { GET_QUEUED_SONGS } from "../../utils/queries";
 import { useQuery } from "@apollo/client";
 import { SongContext } from "../../context/Context";
-import './PlayingNext.css'
+import "./PlayingNext.css";
 import { BsFillPlayFill } from "react-icons/bs";
 import { HiPause } from "react-icons/hi2";
 import { TbChevronsLeft, TbChevronsRight } from "react-icons/tb";
-import Queue from '../Queue/Queue';
- 
-function PlayingNext() {
-    const { state, dispatch } = useContext(SongContext);
-    // const { data } = useQuery(GET_QUEUED_SONGS);
+import Queue from "../Queue/Queue";
+import ReactPlayer from "react-player";
 
-    function handleTogglePlay() {
+function PlayingNext() {
+  const { state, dispatch } = useContext(SongContext);
+  // const { data } = useQuery(GET_QUEUED_SONGS);
+  const [play, setPlay] = useState(0);
+  const [sliding, setSliding] = useState(false);
+  const [playedSeconds, setPlayedSeconds] = useState(0);
+  const reactPlayerRef = useRef();
+  
+
+  function handleTogglePlay() {
     dispatch(state.isPlaying ? { type: "PAUSE_SONG" } : { type: "PLAY_SONG" });
   }
 
+  function handleProgressChange(event, newValue) {
+    console.log(event, 'event')
+    console.log(newValue, 'new value')
+    setPlay(newValue);
+  }
+
+  function handleMouseDown() {
+    setSliding(true);
+  }
+
+  function handleMouseUp() {
+    setSliding(false);
+    console.log(play, "idkk idk")
+    reactPlayerRef.current.seekTo(play);
+  }
+
+
   return (
-    <div className='playing-container'>
-    <img src={state.song.Thumbnail} alt='song'/>
-    <section className='playing-song-info'>
+    <div className="playing-container">
+      <img src={state.song.Thumbnail} alt="song" />
+      <section className="playing-song-info">
         <h4>{state.song.Title}</h4>
         <p>{state.song.Artist}</p>
-    </section>
-    <section className='controls'>
-        <TbChevronsLeft className='playing-btns'/>
-            {
-            state.isPlaying 
-            ? <HiPause onClick={handleTogglePlay} className='playing-btns'/>
-            : <BsFillPlayFill onClick={handleTogglePlay} className='playing-btns'/> 
-            }
-        <TbChevronsRight className='playing-btns'/>
-    </section>
-    {/* <Queue queue={data}/> */}
+      </section>
+      <section className="controls">
+        <TbChevronsLeft className="playing-btns" />
+        {state.isPlaying ? (
+          <HiPause onClick={handleTogglePlay} className="playing-btns" />
+        ) : (
+          <BsFillPlayFill onClick={handleTogglePlay} className="playing-btns" />
+        )}
+        <TbChevronsRight className="playing-btns" />
+      </section>
+      <input 
+       value={play}
+       type="range"
+       min={0}
+       max={1}
+       step={0.01}
+       onChange={handleProgressChange}
+       onMouseDown={handleMouseDown}
+       onMouseUp={handleMouseUp}
+     />
+      <ReactPlayer
+        hidden
+        url={state.song.URL}
+        playing={state.isPlaying}
+        ref={reactPlayerRef}
+        onProgress={({ played, playedSeconds }) => {
+          if (!sliding) {
+            setPlay(played)
+          }
+          console.log(play)
+        }}
+      />
+      {/* <Queue queue={data}/> */}
     </div>
-  )
+  );
 }
 
-export default PlayingNext
+export default PlayingNext;
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -5,8 +5,8 @@ export const SongContext = createContext({
     ID: "12f90577-3709-4651-929f-f96e5c01e116",
     Title: " (세븐틴) 'Rock with you' Official MV",
     Artist: "SEVENTEEN",
-    Thumbnail: "http://img.youtube.com/vi/WpuatuzSDK4/0.jpg",
-    URL: "https://www.youtube.com/watch?v=gRnuFC4Ualw&list=RDEM5NHTZtEX3GlvWCo6kcmjKw&start_radio=1&rv=0lapF4DQPKQ",
+    Thumbnail: "https://img.youtube.com/vi/WpuatuzSDK4/0.jpg",
+    URL:"https://www.youtube.com/watch?v=gRnuFC4Ualw&list=RDEM5NHTZtEX3GlvWCo6kcmjKw&start_radio=1&rv=0lapF4DQPKQ",
     Duration: 184,
   },
 });
