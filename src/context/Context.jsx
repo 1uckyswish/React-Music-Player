8417de67d503a18bb2a5881633a986070d743605
Noel Guillen <noelblas274@gmail.com>
2023-11-07 22:31:47 -0500

    changed thumbnail for default image

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -5,7 +5,7 @@ export const SongContext = createContext({
     ID: "85f07bd5-29e5-40f1-98c2-feb1239d1291",
     Title: "Somewhere Only We Know",
     Artist: "Keane",
-    Thumbnail: "https://images.pexels.com/photos/6536121/pexels-photo-6536121.jpeg?auto=compress&cs=tinysrgb&w=1200",
+    Thumbnail: "https://images.pexels.com/photos/13884190/pexels-photo-13884190.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     URL:"https://www.youtube.com/watch?v=-HwPKDlb3e8",
     Duration: 234,
   },
